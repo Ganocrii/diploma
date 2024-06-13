7c740e109d2ae07c4125e2cc76f18cbab85ef0ed
unknown <aleks123kos@gmail.com>
2024-06-13 14:55:50 +0600

    finish line2

--- server/server.js
+++ server/server.js
@@ -94,13 +94,10 @@ app.get('/api/products', async (req, res) => {
   }
 });
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('client/build'));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.use(express.static('client/build'));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
