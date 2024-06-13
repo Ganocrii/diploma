455bdcdd9f9b8f6921a3f260e93fc5e5da7f8acb
unknown <aleks123kos@gmail.com>
2024-06-13 14:21:45 +0600

    finishline1

--- server/server.js
+++ server/server.js
@@ -94,6 +94,14 @@ app.get('/api/products', async (req, res) => {
   }
 });
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
