d3b1f447407aa3bc0c35c8b55d58fdb52e35d1fe
unknown <aleks123kos@gmail.com>
2024-06-15 16:24:42 +0600

    123456

--- server/server.js
+++ server/server.js
@@ -13,14 +13,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// Serve static files from the React app
-app.use(express.static(path.join(__dirname, 'client/build')));
-
-// The "catchall" handler: for any request that doesn't match the above, send back React's index.html file.
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client/build/index.html'));
-});
-
 const uri = process.env.MONGODB_URI;
 
 if (!uri) {
