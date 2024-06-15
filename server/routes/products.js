f20ee741c3393d875774146cfe06e7a23797602e
unknown <aleks123kos@gmail.com>
2024-06-15 23:21:45 +0600

    updated products.js

--- server/routes/products.js
+++ server/routes/products.js
@@ -1,43 +1,37 @@
 const express = require('express');
-const Product = require('../models/Product');
+const router = express.Router();
 
 module.exports = (db) => {
-  const router = express.Router();
+  const Product = db.collection('products');
 
   router.get('/', async (req, res) => {
     try {
       const { businessType, sectors, selectionType, configType } = req.query;
       console.log('Query:', req.query);
 
-      const collection = db.collection('products');
-
-      const parsedSectors = Array.isArray(sectors) ? sectors : [sectors];
       const softwareQuery = {
-        sectors: { $in: [...parsedSectors, 'all'] },
+        sectors: { '$in': [sectors, 'all'] },
         type: 'software'
       };
-      const hardwareQuery = {
-        sectors: { $in: [...parsedSectors, 'all'] },
-        type: 'hardware'
-      };
 
-      if (configType) {
-        hardwareQuery.performance = configType === 'low' ? 'low' : 'high';
-      }
+      const hardwareQuery = {
+        sectors: { '$in': [sectors, 'all'] },
+        type: 'hardware',
+        performance: configType
+      };
 
       console.log('Software Query:', softwareQuery);
       console.log('Hardware Query:', hardwareQuery);
 
-      const softwareProducts = await collection.find(softwareQuery).toArray();
-      const hardwareProducts = await collection.find(hardwareQuery).toArray();
-      const products = [...softwareProducts, ...hardwareProducts];
+      const softwareProducts = await Product.find(softwareQuery).toArray();
+      const hardwareProducts = await Product.find(hardwareQuery).toArray();
 
+      const products = [...softwareProducts, ...hardwareProducts];
       console.log('Products:', products);
-      //console.log('Sending response:', JSON.stringify(products));
       res.json(products);
-    } catch (err) {
-      console.error('Error fetching products:', err);
-      res.status(500).send(err);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+      res.status(500).send('Server error');
     }
   });
 
