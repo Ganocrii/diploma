a3bab8826b278c8b1c89b8ec635191ed2e78f342
unknown <aleks123kos@gmail.com>
2024-06-15 13:58:40 +0600

    Final changes for deploy!

--- server/server.js
+++ server/server.js
@@ -1,28 +1,48 @@
 const express = require('express');
-const mongoose = require('mongoose');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const cors = require('cors');
-const dotenv = require('dotenv');
+const bodyParser = require('body-parser');
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const productsRouter = require('./routes/products');
-
-dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(cors());
-app.use(express.json());
-app.use('/api/products', productsRouter);
+const uri = process.env.MONGODB_URI;
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected successfully to MongoDB');
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  })
-  .catch(err => {
-    console.error('Failed to connect to MongoDB', err);
-  });
+if (!uri) {
+  console.error('Error: MONGODB_URI is not defined in .env file');
+  process.exit(1);
+}
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+let db;
+
+async function connectDB() {
+  try {
+    await client.connect();
+    console.log("Connected successfully to MongoDB");
+    db = client.db('projectdb');
+    // Инициализируем маршрутизатор после подключения к базе данных
+    app.use('/api/products', productsRouter(db));
+  } catch (err) {
+    console.error('Error connecting to MongoDB:', err.message);
+  }
+}
+
+connectDB().catch(console.dir);
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
